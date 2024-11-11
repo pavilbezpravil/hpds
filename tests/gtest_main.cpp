edc3273dedd8b9c9d715787c6a6037f94efa1eab
Pavel <pavilbezpravil@gmail.com>
2024-11-12 01:18:42 +0400

    PoolAllocator

--- tests/gtest_main.cpp
+++ tests/gtest_main.cpp
@@ -62,6 +62,128 @@ TEST(IndexPool, Basic) {
    ASSERT_EQ(pool.Available(), 3);
 }
 
+#include <memory>
+#include <iostream>
+#include <vector>
+
+template<typename T>
+class SimpleAllocator {
+public:
+   using value_type = T;
+
+   SimpleAllocator() = default;
+   // SimpleAllocator(const SimpleAllocator&) = default;
+   // SimpleAllocator(SimpleAllocator&&) = default;
+   //
+   // SimpleAllocator& operator=(const SimpleAllocator&) = default;
+   // SimpleAllocator& operator=(SimpleAllocator&&) = default;
+
+   template<typename U>
+   constexpr SimpleAllocator(const SimpleAllocator<U>&) noexcept {}
+
+   T* allocate(std::size_t n) {
+      std::cout << "Allocating " << n * sizeof(T) << " bytes" << std::endl;
+      if (n > std::allocator_traits<SimpleAllocator>::max_size(*this)) {
+         throw std::bad_alloc();
+      }
+      return static_cast<T*>(::operator new(n * sizeof(T)));
+   }
+
+   void deallocate(T* p, std::size_t n) noexcept {
+      std::cout << "Deallocating memory" << std::endl;
+      ::operator delete(p);
+   }
+
+   // template<typename U, typename... Args>
+   // void construct(U* p, Args&&... args) {
+   //    std::cout << "Constructing element" << std::endl;
+   //    new(p) U(std::forward<Args>(args)...);
+   // }
+   //
+   // template<typename U>
+   // void destroy(U* p) noexcept {
+   //    std::cout << "Destroying element" << std::endl;
+   //    p->~U();
+   // }
+   //
+   // friend bool operator==(const SimpleAllocator&, const SimpleAllocator&) { return true; }
+   // friend bool operator!=(const SimpleAllocator&, const SimpleAllocator&) { return false; }
+};
+
+template<typename T>
+class PoolAllocator {
+public:
+   using value_type = T;
+
+   // PoolAllocator() = default;
+   PoolAllocator() {
+      std::cout << "Allocator create for " << sizeof(T) << " bytes" << std::endl;
+   }
+   ~PoolAllocator() {
+      std::cout << "Allocator destroyed for " << sizeof(T) << " bytes" << std::endl;
+   }
+
+   PoolAllocator(const PoolAllocator&) = default;
+   PoolAllocator(PoolAllocator&&) = default;
+   
+   PoolAllocator& operator=(const PoolAllocator&) = default;
+   PoolAllocator& operator=(PoolAllocator&&) = default;
+
+   template<typename U>
+   constexpr PoolAllocator(const PoolAllocator<U>&) noexcept {}
+
+   T* allocate(std::size_t n) {
+      std::cout << "Allocating " << n * sizeof(T) << " bytes" << std::endl;
+
+      assert(n == 1 && "PoolAllocatorWrapper can only allocate one element at a time");
+      if (m_freeList.empty()) {
+         allocateMemory(m_blockElementCount);
+      }
+      
+      void* ptr = m_freeList.back();
+      m_freeList.pop_back();
+      return static_cast<T*>(ptr);
+   }
+
+   void deallocate(T* p, std::size_t n) noexcept {
+      std::cout << "Deallocating memory" << std::endl;
+
+      assert(n == 1 && "PoolAllocatorWrapper can only deallocate one element at a time");
+      m_freeList.push_back(p);
+   }
+
+private:
+   std::vector<std::unique_ptr<uint8_t[]>> m_blocks;
+   std::size_t m_blockElementCount = 64;
+   std::vector<void*> m_freeList;
+
+   void allocateMemory(std::size_t elementCount) {
+      constexpr std::size_t m_elementSize = sizeof(T);
+
+      auto newBlock = std::make_unique<uint8_t[]>(m_elementSize * elementCount);
+      uint8_t* rawBlock = newBlock.get();
+      m_blocks.push_back(std::move(newBlock));
+      for (std::size_t i = 0; i < elementCount; ++i) {
+         void* ptr = rawBlock + i * m_elementSize;
+         m_freeList.push_back(ptr);
+      }
+   }
+};
+
+TEST(Allocator, Basic) {
+   // std::map<int, int, std::less<>, SimpleAllocator<std::pair<const int, int>>> map;
+   std::map<int, int, std::less<>, PoolAllocator<std::pair<const int, int>>> map;
+   map[0] = 42;
+   map[1] = 137;
+   map[2] = 228;
+   map[3] = 31415;
+
+   ASSERT_EQ(map[0], 42);
+   ASSERT_EQ(map[1], 137);
+
+   map.clear();
+}
+
 int main(int argc, char** argv) {
    ::testing::InitGoogleTest(&argc, argv);
    return RUN_ALL_TESTS();
